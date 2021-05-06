5e60550c1d2aa754dd195495cb6dd4475da41162
Gurvir Uppal <gurviruppal@icloud.com>
2021-05-06 10:19:48 -0400

    Add project files.

---
+++ averageValues/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace averageValues
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addButton = new System.Windows.Forms.Button();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.titleHeader = new System.Windows.Forms.Label();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // addButton
+            // 
+            this.addButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.addButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.addButton.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addButton.Location = new System.Drawing.Point(321, 106);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(65, 43);
+            this.addButton.TabIndex = 0;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // inputBox
+            // 
+            this.inputBox.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputBox.Location = new System.Drawing.Point(183, 118);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(88, 26);
+            this.inputBox.TabIndex = 1;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.Color.White;
+            this.outputLabel.Font = new System.Drawing.Font("Mongolian Baiti", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(102, 198);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(312, 69);
+            this.outputLabel.TabIndex = 2;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // titleHeader
+            // 
+            this.titleHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.titleHeader.Font = new System.Drawing.Font("Mongolian Baiti", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleHeader.Location = new System.Drawing.Point(0, -1);
+            this.titleHeader.Name = "titleHeader";
+            this.titleHeader.Size = new System.Drawing.Size(502, 63);
+            this.titleHeader.TabIndex = 3;
+            this.titleHeader.Text = "Average Number Solver";
+            this.titleHeader.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // resetButton
+            // 
+            this.resetButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.resetButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.resetButton.Location = new System.Drawing.Point(414, 332);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 23);
+            this.resetButton.TabIndex = 4;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(501, 376);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.titleHeader);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.addButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label titleHeader;
+        private System.Windows.Forms.Button resetButton;
+    }
+}
+
---
+++ averageValues/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace averageValues
+{
+    public partial class Form1 : Form
+    {
+        double sum;
+        double average;
+
+        int counter;
+        int numb;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                numb = Convert.ToInt32(inputBox.Text);
+
+                if (numb != 0)
+                {
+                    counter++;                                                  //add to the counter every single time the button is pressed
+                    sum += numb;                                                //add the sum and the new number
+                    outputLabel.Text = $"{numb} was added to the sum";
+                }
+                else 
+                {
+                     average = sum / counter;                                                           //divide the sum by the counter to get the aveage
+                    outputLabel.Text = $"The avaerage of all numbers enetered is: {average.ToString("#.##")}";
+                }
+            }
+            catch
+            {
+                outputLabel.Text = "Pleae input a number";
+            }
+            inputBox.Clear();
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            sum = 0;
+            average = 0;
+            counter = 0;
+
+            outputLabel.Text = "";
+        }
+    }
+
+
+
+
+
+
+}
